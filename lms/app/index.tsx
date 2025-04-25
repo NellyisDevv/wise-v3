b431657bc2728a25488733fc17a059d5d9373db2
Nelson School <NelsonSchool@Nelsons-Work-MacBook-Pro.local>
2025-04-25 15:54:25 -0400

    Initial commit to app

---
+++ lms/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+
+export default function Index() {
+  return (
+    <View className='flex-1 justify-center items-center'>
+      <Text className='text-black text-xl'>Something big coming soon! 🤓</Text>
+      <Text className='text-sm mt-2'>More coming soon!</Text>
+    </View>
+  )
+}
