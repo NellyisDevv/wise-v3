4ed49b0c91f2802e5d9a60ab8260343defbfb7ce
Nelson School <NelsonSchool@Nelsons-Work-MacBook-Pro.local>
2025-04-25 16:05:56 -0400

    tailwind working!

--- lms/app/index.tsx
+++ lms/app/index.tsx
@@ -1,10 +1,13 @@
-import { Text, View } from 'react-native'
+import { Text, View, Pressable } from 'react-native'
 
 export default function Index() {
   return (
     <View className='flex-1 justify-center items-center'>
       <Text className='text-black text-xl'>Something big coming soon! 🤓</Text>
       <Text className='text-sm mt-2'>More coming soon!</Text>
+      <Pressable className='bg-noir p-3 rounded-md mt-4'>
+        <Text className='text-sm text-white font-semibold'>Get notified</Text>
+      </Pressable>
     </View>
   )
 }
