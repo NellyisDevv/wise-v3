07f523b878c1c75990b646d9a6eb6780557ee958
Nelson School <NelsonSchool@Nelsons-Work-MacBook-Pro.local>
2025-04-26 18:41:40 -0400

    removing bun

--- lms/app/_layout.tsx
+++ lms/app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Slot } from 'expo-router'
 import '@/global.css'
+import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { Slot } from 'expo-router'
+import { LogBox } from 'react-native'
+import { tokenCache } from '@/utils/cache'
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error(
+    'Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env'
+  )
+}
+LogBox.ignoreLogs(['Clerk: Clerk has been loaded with development keys'])
 
 export default function RootLayout() {
-  return <Slot />
+  return (
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Slot />
+      </ClerkLoaded>
+    </ClerkProvider>
+  )
 }
